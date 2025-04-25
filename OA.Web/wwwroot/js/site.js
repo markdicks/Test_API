ffba169a748b9bc00ec885d6b95f18d007f81646
markdicks <markdicks03@gmail.com>
2025-04-25 16:37:45 +0200

    Fix flashing when switching pages on different theme

--- OA.Web/wwwroot/js/site.js
+++ OA.Web/wwwroot/js/site.js
@@ -1,20 +1,20 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
-    const toggleBtn = document.getElementById('themeToggle');
-    const body = document.body;
+﻿//document.addEventListener('DOMContentLoaded', function () {
+    //const toggleBtn = document.getElementById('themeToggle');
+    //const body = document.body;
 
     // Load saved theme
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme) {
-        body.classList.add(savedTheme);
-    } else {
-        body.classList.add('dark-mode'); // default
-    }
+    //const savedTheme = localStorage.getItem('theme');
+    //if (savedTheme) {
+    //    body.classList.add(savedTheme);
+    //} else {
+    //    body.classList.add('dark-mode'); // default
+    //}
 
-    toggleBtn.addEventListener('click', () => {
-        body.classList.toggle('dark-mode');
-        body.classList.toggle('light-mode');
+    //toggleBtn.addEventListener('click', () => {
+    //    body.classList.toggle('dark-mode');
+    //    body.classList.toggle('light-mode');
 
-        const newTheme = body.classList.contains('dark-mode') ? 'dark-mode' : 'light-mode';
-        localStorage.setItem('theme', newTheme);
-    });
-});
+    //    const newTheme = body.classList.contains('dark-mode') ? 'dark-mode' : 'light-mode';
+    //    localStorage.setItem('theme', newTheme);
+    //});
+//});
